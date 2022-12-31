7d4ecd38eba9627b31f7c71008a89990b02ed77b
Ishara Maduranga <99071775+isharamaduranga@users.noreply.github.com>
2022-12-31 16:09:31 +0530

    content updated for add all details add to object and after added JSON Array to send LocalStorage...

--- js/script.js
+++ js/script.js
@@ -1,12 +1,29 @@
-
-window.addEventListener('load',() => {
-    todos  = JSON.parse(localStorage.getItem('todos')) || [];
+window.addEventListener('load', () => {
+    todos = JSON.parse(localStorage.getItem('todos')) || [];
     const nameInput = document.querySelector('#name');
-    const nameTodoForm = document.querySelector('#new-todo-form');
+    const newTodoForm = document.querySelector('#new-todo-form');
 
-    const username =localStorage.getItem('username') || '';
-    nameInput.value=username;
-    nameInput.addEventListener('change',evt => {
-        localStorage.setItem('username',evt.target.value);
+    const username = localStorage.getItem('username') || '';
+    nameInput.value = username;
+    nameInput.addEventListener('change', evt => {
+        localStorage.setItem('username', evt.target.value);
+    });
+
+    newTodoForm.addEventListener('submit', evt => {
+        evt.preventDefault();
+
+        const todo = {
+            content: evt.target.elements.content.value,
+            category: evt.target.elements.category.value,
+            done: false,
+            createdAt: new Date().getTime()
+        }
+
+        todos.push(todo);
+
+        localStorage.setItem('todos',JSON.stringify(todos));
+
+        evt.target.reset();
+
     })
 });
